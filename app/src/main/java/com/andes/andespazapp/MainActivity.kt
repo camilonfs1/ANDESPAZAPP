b1d508e815e2a774fd2d9b814ebe0339723b5316
camilonfs1 <camilonfs1@gmail.com>
2020-07-30 22:51:06 -0500

    splash and login

--- app/src/main/java/com/andes/andespazapp/MainActivity.kt
+++ app/src/main/java/com/andes/andespazapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.andes.andespazapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
     }
 }
