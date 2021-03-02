cb5ebff34e0ac16e5db36d9a845c34fffca5d0fc
camilonfs1 <camilonfs1@gmail.com>
2021-03-01 21:24:55 -0500

    complaints

--- app/src/main/java/com/andes/andespazapp/View/Complaints/Sent_Complaint.kt
+++ app/src/main/java/com/andes/andespazapp/View/Complaints/Sent_Complaint.kt
@@ -2,7 +2,11 @@ package com.andes.andespazapp.View.Complaints
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.andes.andespazapp.Model.Complaint
+import com.andes.andespazapp.Model.Firebase.Compaint_firebase
 import com.andes.andespazapp.R
 import kotlinx.android.synthetic.main.activity_sent__complaint.*
 
@@ -11,10 +15,27 @@ class Sent_Complaint : AppCompatActivity() {
     var txt:EditText?=null
     var contact:EditText?=null
 
+    var btn: Button?=null
+
+    var firebase = Compaint_firebase()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sent__complaint)
         txt = text_complaint
+        contact = txt_contact
+        btn=btn_complaint
+
+        btn!!.setOnClickListener {
+            var complaint = Complaint(txt!!.text.toString(),contact!!.text.toString())
+            firebase.new_complaint(complaint)
+            clear()
+            Toast.makeText(this,"GRACIAS", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun clear(){
+        txt!!.text.clear()
+        contact!!.text.clear()
     }
 }
