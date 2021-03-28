55ab16441655cd007289cf467e15cf5720e2ad01
camilonfs1 <camilonfs1@gmail.com>
2021-03-28 18:50:55 -0500

    paz

--- app/src/main/java/com/andes/andespazapp/View/Learn/Peace/MainPeace.kt
+++ app/src/main/java/com/andes/andespazapp/View/Learn/Peace/MainPeace.kt
@@ -1,12 +1,60 @@
 package com.andes.andespazapp.View.Learn.Peace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import com.andes.andespazapp.R
+import com.andes.andespazapp.View.Learn.Diversity.SchoolDiversity
+import kotlinx.android.synthetic.main.activity_main_peace.*
 
 class MainPeace : AppCompatActivity() {
+    private var card1:CardView?=null
+    private var card2:CardView?=null
+    private var card3:CardView?=null
+    private var card4:CardView?=null
+    private var card5:CardView?=null
+    private var card6:CardView?=null
+    private var card7:CardView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_peace)
+
+        card1=card_tipos
+        card2=card_violenciacolombia
+        card3=card_acuerdosdepaz
+        card4=card_acuerdoshistoria
+        card5=card_simbolos
+        card6=card_importancia
+        card7=card_activity
+
+        card1!!.setOnClickListener {
+            var intent = Intent(this, TiposDeViolencia::class.java)
+            this.startActivity(intent)
+        }
+        card2!!.setOnClickListener {
+            var intent = Intent(this, ViolenciaEnColombia::class.java)
+            this.startActivity(intent)
+        }
+        card3!!.setOnClickListener {
+            var intent = Intent(this, AcuerdoDePazEnColombia::class.java)
+            this.startActivity(intent)
+        }
+        card4!!.setOnClickListener {
+            var intent = Intent(this, AcuerdoDePazEnColombia::class.java)
+            this.startActivity(intent)
+        }
+        card5!!.setOnClickListener {
+            var intent = Intent(this, SimbolosPaz::class.java)
+            this.startActivity(intent)
+        }
+        card6!!.setOnClickListener {
+
+        }
+        card7!!.setOnClickListener {
+            var intent = Intent(this, ActivityPeace::class.java)
+            this.startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/andes/andespazapp/View/Learn/Peace/SimbolosPaz.kt
@@ -0,0 +1,12 @@
+package com.andes.andespazapp.View.Learn.Peace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.andes.andespazapp.R
+
+class SimbolosPaz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simbolos_paz)
+    }
+}
